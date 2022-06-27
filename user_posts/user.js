44ac0c187d0069246cad0841e51b564f87169170
Wilson <wncoding@gmail.com>
2022-06-27 16:17:55 +1000

    user-posts - js/api practice

---
+++ user_posts/user.js
@@ -0,0 +1,23 @@
+const post_listEL = document.querySelector(".post-list")
+
+async function main() {
+    const id = localStorage.getItem("id")
+    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
+    const postsData = await posts.json()
+    console.log(postsData)
+
+    post_listEL.innerHTML = postsData.map(post => postHTML(post)).join('')
+}
+
+main()
+
+function postHTML(post) {
+    return `<div class="post">
+                <div class="post__title">
+                ${post.title}
+                </div>
+                <p class="post__body">
+                ${post.body}
+                </p>
+            </div>`
+}
