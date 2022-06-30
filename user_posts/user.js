477068d4cefcfaa42c43962c4500188e13ec2189
Wilson <wncoding@gmail.com>
2022-06-30 19:52:21 +1000

    Finish user-posts js prep

--- user_posts/user.js
+++ user_posts/user.js
@@ -1,15 +1,18 @@
 const post_listEL = document.querySelector(".post-list")
+const id = localStorage.getItem("id")
 
-async function main() {
-    const id = localStorage.getItem("id")
-    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
+async function renderPosts(userId) {
+    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
     const postsData = await posts.json()
-    console.log(postsData)
+    //console.log(postsData)
 
     post_listEL.innerHTML = postsData.map(post => postHTML(post)).join('')
 }
 
-main()
+async function onSearchChange(event) {
+    const id = event.target.value
+    renderPosts(id)
+}
 
 function postHTML(post) {
     return `<div class="post">
@@ -21,3 +24,5 @@ function postHTML(post) {
                 </p>
             </div>`
 }
+
+renderPosts(id)
